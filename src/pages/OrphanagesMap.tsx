6a6841b395c66ca2264f5e5eabb4a26215f2695f
Fabricyo Barreto <bass.barreto@gmail.com>
2020-10-28 09:04:50 -0300

    code abstraction was made in a component and a function.

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -2,19 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import Leaflet from 'leaflet';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
-import 'leaflet/dist/leaflet.css';
 import '../styles/pages/orphanages-map.css';
+import mapIcon from '../utils/mapIcon';
 
-const mapIcon = Leaflet.icon({
-  iconUrl: mapMarkerImg,
-  iconSize: [50, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [170, 2]
-})
+
 
 const OrphanagesMap: React.FC = () => {
   return (
